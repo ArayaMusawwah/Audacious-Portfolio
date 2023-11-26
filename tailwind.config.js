3ffcb9cb13cddb4356b96fd5cff7f32b6b1bd1f0
Araya Musawwah <miramadhan354@gmail.com>
2023-11-27 00:09:12 +0700

    Smooth Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.{html,js}'],
+  content: ["*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        'poppins': ['Poppins', 'sans-serif'],
-        'roboto': ['Roboto', 'sans-serif']
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
 
-      transitionTimingFunction:{
-        'custom-1': 'cubic-bezier(0.080, 0.9, 0.118, 1)'
-      }
+      transitionTimingFunction: {
+        "custom-1": "cubic-bezier(0.080, 0.9, 0.118, 1)",
+      },
     },
   },
   plugins: [],
-}
-
+};
